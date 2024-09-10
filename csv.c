d8183284a1ddd41d6128f5281f5d633f1c53cc02
Joe Alessi <jjalessi@buffalo.edu>
2024-09-10 12:10:35 -0400

    added main function to test.c along with function to read csv file in preparation of processing

--- csv.c
+++ csv.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// function to open file
+FILE* open_file(int argc, char *argv[]) {
+
+    char *file = argv[argc - 1]; // get name of file
+
+    FILE *f = fopen(file, "r"); // open file for reading
+
+    if (f == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    return f;
+}
 
 int main(int argc, char *argv[]) {
 
